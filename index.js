967034876ec19a556247e581bf4c3df4b127b5d3
waaizulfahmi <wandaaizulf@gmail.com>
2023-11-25 21:39:25 +0700

    update cors

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.use("/api/notification", require("./routes/api/notification"));
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log("server started"));
+
 const express = require("express");
+const cors = require("cors"); // Import middleware CORS
 
 const app = express();
+
+// Gunakan middleware CORS
+app.use(cors());
 
 app.use("/api/notification", require("./routes/api/notification"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log("server started"));
+app.listen(PORT, () => console.log("Server started on port", PORT));
